84dab7c446a3adfebe6353040310855fc8107dd8
Demetrios <demetriospagonis@weber.edu>
2024-05-18 11:42:42 -0600

    v0.0.1

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chemical_safety",
+    version="0.0.1",
+    packages=find_packages(include=['chemical_safety', 'chemical_safety.*']),
+    description="A package for retreiving chemical safety information",
+    author="Demetrios Pagonis",
+    author_email="demetriospagonis@weber.edu",
+    install_requires=[
+        'pandas',
+        'numpy',
+        'jinja2',
+        'requests',
+        'Levenshtein',
+        'natsort',
+        'flask'
+    ],
+    entry_points={
+        'console_scripts': [
+            'chemical-dashboard=dashboard.app:chemical_dashboard',
+        ],
+    },
+)
